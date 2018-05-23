907ae85fda93ae7161131e30a306a20f034cb000
TOSHIUBA\Brenda <brendamorpe@gmail.com>
2018-05-23 13:16:47 -0500

    2da prueba

--- Trivia/main.js
+++ Trivia/main.js
@@ -7,7 +7,30 @@ if (nombre == null || nombre == "") {
         mensaje = "Bienvenida";
         } else {
             mensaje = "Bienvenida " + nombre;
+            var gameplay = prompt ('¿Quieres jugar SI O NO?');
+            if (gameplay == ("SI") || ("si"))     {
+            var firstQuestion = prompt('¿Cuanto es 2+ 2?')
+          }
+
+
+
+if (firstQuestion == ("4")) {
+// Si contesta correctamente mandarle la segunda pregunta
+var secondQuestion = prompt ("¿Cuanto es 5*1?")
+}
+if(secondQuestion == ("5")){
+  // Si contesta correctamente mandarle la tercer pregunta
+  var thirdQuestion= prompt ("¿Cuanto es 1000-500?")
+}
+  if(thirdQuestion == "500"){
+    alert("Vamos a ver tu resultado!!!!!!!")
+            }else {
+
+              alert("Respuesta Incorrecta");
             }
-            document.getElementById("usuario").innerHTML = mensaje;
-}else{
-  var nombre = prompt("Lista para jugar?:");
+
+
+
+          }
+      document.getElementById("usuario").innerHTML = mensaje;
+    }
